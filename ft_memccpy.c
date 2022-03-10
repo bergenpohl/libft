4e64bd5c6d982dde2e46c5b249a741e2f646c606
Bergen Pohl <circidalia@gmail.com>
2022-03-09 22:04:03 -0500

    Update ft_memccpy.c

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -12,13 +12,13 @@
 
 #include "libft.h"
 
-void	*ft_memccpy(void *restrict dst,
-					const void *restrict src,
-					int c, size_t n)
+void	*ft_memccpy(void	*restrict dst,
+		    		const void *restrict src,
+		    		int c, size_t n)
 {
 	unsigned char	*d;
 	unsigned char	*s;
-	size_t			i;
+	size_t		i;
 	unsigned char	stop;
 
 	if (!dst && !src)
